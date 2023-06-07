6e60208a06607c6ad54e49e2c55449a9681a9f94
paglobal <jackamoah.pa@gmail.com>
2023-06-07 12:55:37 +0000

    setup complete...at least for now

--- trackit-server/index.js
+++ trackit-server/index.js
@@ -1,1 +1,11 @@
-console.log("Awuraaa!!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send(JSON.stringify("I am here!!!")));
+
+app.get("/awuraa", (req, res) =>
+  res.send(JSON.stringify("This is the url endpoint for Awuraaa!!!"))
+);
+
+app.listen("4000", () => console.log("I'm running!!!"));
